131ffb09c73349e9f45193f875736208d8f398ae
Mario Adamo Jr <marioadamojr@gmail.com>
2019-10-29 22:35:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,10 +18,11 @@ function moveDodgerRight() {
   let rightNumbers = dodger.style.left.replace("px", "");
   let right = parseInt(rightNumbers, 10);
 
-  if (right < 0) {
+  if (left > 0) {
     dodger.style.left = `${right + 1}px`;
   }
 }
+
 
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
