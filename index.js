59dacec4a122309ef23389ee1190d3bcbf2c6a21
Mario Adamo Jr <marioadamojr@gmail.com>
2019-10-29 22:05:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
 
 dodger.style.bottom = "0px";
+
+document.addEventListener("keydown", function(e) {
+  console.log(e.key);
+});
