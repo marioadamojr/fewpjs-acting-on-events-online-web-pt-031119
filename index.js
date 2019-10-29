2951887b380c948bd7056cab920b84ead750621a
Mario Adamo Jr <marioadamojr@gmail.com>
2019-10-29 22:30:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function moveDodgerRight() {
   let rightNumbers = dodger.style.left.replace("px", "");
   let right = parseInt(rightNumbers, 10);
 
-  if (left < 0) {
+  if (right < 0) {
     dodger.style.left = `${right + 1}px`;
   }
 }
