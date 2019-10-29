309d64e761b7fe53f40303e86be9d4d82faaa4b1
Mario Adamo Jr <marioadamojr@gmail.com>
2019-10-29 22:10:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ dodger.style.backgroundColor = "#FF69B4";
 dodger.style.bottom = "0px";
 
 document.addEventListener("keydown", function(e) {
-  console.log(e.key);
+  if (e.key === "ArrowLeft") {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    dodger.style.left = `${left - 1}px`;
+  }
 });
